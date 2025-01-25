1ecbf7d2e8d13ba1d2146efe4438b340c763c7f0
Ryan Boustany <rboustany@atlassian.com>
2025-01-25 13:05:21 +1100

    basic app with title created

---
+++ sitemate-news/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sitemate News</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
